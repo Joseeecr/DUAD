fa414a0533d9bb2aa99023cacc16fbf923586559
Joseeecr <blancomiguelj@gmail.com>
2024-09-14 17:19:53 -0600

    Changes requested by Eduardo

--- semana 13/is_all_num.py
+++ semana 13/is_all_num.py
@@ -1,20 +1,15 @@
 def print_parameters(func):
     def wrapper(*args):
-        try:
-            for number in args:
-                int(number)
-                print(number)
-        except ValueError as ex:
-            print(f"An error has been ocurred. Error : {ex}")
-        func(*args)
-
+        if not all(isinstance(numb, (int, float)) for numb in args):
+            raise TypeError("All parameters must be numbers!")
+        return func(*args)
     return wrapper
 
 @print_parameters
 def only_numbers(*args):
-    pass
+    return args
 
 
 
-only_numbers(1,1,3,4,5,6)
-only_numbers(1, "hello")
+print(only_numbers(1,1,3,4,5,6))
+print(only_numbers(1, "hello"))
