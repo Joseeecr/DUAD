ac830c719dacbd2c91caabd6011e85c87479f140
Joseeecr <blancomiguelj@gmail.com>
2025-05-11 11:07:26 -0600

    Added one category/products join and more queries for the categories table

--- backend project/database/queries/products/read_products.sql
+++ backend project/database/queries/products/read_products.sql
@@ -1,2 +1,16 @@
+--get all products
 SELECT id, name, price, category_id, sku, stock, created_date
 FROM pets_eccomerce.products ORDER BY id ASC;
+
+-- get products based on SKU. Replace :SKU with SKU
+SELECT id, name, price, category_id, sku, stock, created_date
+FROM pets_eccomerce.products  WHERE sku = :sku;
+
+-- get products based on name. Replace :name with name
+SELECT id, name, price, category_id, sku, stock, created_date
+FROM pets_eccomerce.products  WHERE name = :name;
+
+-- get products based on a range of dates. Replace :created_date with created_date
+SELECT id, name, price, category_id, sku, stock, created_date
+FROM pets_eccomerce.products  WHERE created_date BETWEEN :created_date AND :created_date
+ORDER BY created_date ASC;
