e8eba929d1408dac94abd7c9155aeafdb3848ff4
Joseeecr <blancomiguelj@gmail.com>
2025-09-21 15:15:05 -0600

    refactor invoices_controller to avoid using a global object

--- backend_project/app/controllers/invoices_controller.py
+++ backend_project/app/controllers/invoices_controller.py
@@ -1,37 +1,33 @@
 from flask import request, jsonify, g
-from app.db.database import engine
 from app.exceptions.exceptions import ValidationError, NotFoundError
-from app.services.invoices_services import InvoicesServices
-from app.repos.invoices_repository import InvoicesRepository
-from app.validators.invoices_validators import InvoicesValidator
 
-invoices_validator = InvoicesValidator()
-invoices_repo = InvoicesRepository(engine)
-invoices_service = InvoicesServices(invoices_validator, invoices_repo)
 
 class InvoicesController:
+  def __init__(self, invoices_service):
+    self.invoices_service = invoices_service
 
   def get_invoices(self):
     try:
 
       params = request.args.to_dict()
-      return invoices_service.list_invoices(params)
+      return self.invoices_service.list_invoices(params)
 
     except ValidationError as e:
       return jsonify({"error": str(e)}), 400
     except NotFoundError as e:
       return jsonify({"error": str(e)}), 400
     except Exception as e:
-      return jsonify({"error": str(e)}), 500
-
+      print({"error": str(e)})
+      return jsonify({"error": "Internal Server Error"}), 500
 
   def check_user_invoices(self):
     try:
-      return invoices_service.get_user_invoices(g.user_id)
+      return self.invoices_service.get_user_invoices(g.user_id)
 
     except ValidationError as e:
       return jsonify({"error": str(e)}), 400
     except NotFoundError as e:
       return jsonify({"error": str(e)}), 404
     except Exception as e:
-      return jsonify({"error": str(e)}), 500
+      print({"error": str(e)})
+      return jsonify({"error": "Internal Server Error"}), 500
--- backend_project/app/routes/invoices_routes.py
+++ backend_project/app/routes/invoices_routes.py
@@ -1,11 +1,19 @@
 from flask import Blueprint
+from app.db.database import engine
 from app.controllers.invoices_controller import InvoicesController
 from app.auth.admin_only import admin_only
 from app.controllers.controllers_utils import jwt_required
+from app.services.invoices_services import InvoicesServices
+from app.repos.invoices_repository import InvoicesRepository
+from app.validators.invoices_validators import InvoicesValidator
 
+invoices_validator = InvoicesValidator()
+invoices_repo = InvoicesRepository(engine)
+invoices_service = InvoicesServices(invoices_validator, invoices_repo)
+
+invoices_controller = InvoicesController(invoices_service)
 
 invoices_bp = Blueprint("invoices", __name__, url_prefix="/invoices")
-invoices_controller = InvoicesController()
 
 @invoices_bp.route("/", methods=['GET'])
 @admin_only
