45d50aba127c299337894c4bc06c2c68ae08b0c2
Joseeecr <blancomiguelj@gmail.com>
2025-10-18 10:28:26 -0600

    Remove unnecessary folders from main

--- backend project/app/auth/jwt_instance.py
+++
@@ -1,3 +0,0 @@
-from auth.jwt_manager import JWT_Manager
-
-jwt_manager = JWT_Manager("../private.pem", "../public.pem")
--- backend project/app/db/database.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-from db.config import database_url
-
-engine = create_engine(database_url, echo=True)
