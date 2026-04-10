3e7e09843fb3b681a90c0fc29a0474b25753a461
Jose Blanco <blancomiguelj@gmail.com>
2026-04-09 22:03:17 -0600

    Created login html and js files

---
+++ frontend-requests-exercises/domExercises/project/js/login.js
@@ -0,0 +1,69 @@
+const userInstance = axios.create({
+  baseURL: "https://api.restful-api.dev/",
+  headers:{
+    "x-api-key": "77adaabc-9a83-469f-baaa-9973f9b8a14c"
+  }
+});
+const loginForm = document.getElementById("login-form");
+
+const formatResponse = (success, data, error) => {
+  return {
+    success,
+    data,
+    error
+  }
+}
+
+
+const getUserById = async (userId) => {
+  try {
+    const response = await userInstance.get(`collections/users/objects/${userId}`);
+    
+    return formatResponse(true, response.data, null);
+
+  } catch (error) {
+    if (error.response) {
+      const details = error.response?.data?.error || null
+
+      return formatResponse(false, null, {
+        type: "HTTP_ERROR",
+        message: error.response.statusText,
+        status: error.response.status,
+        details: details
+      });
+    }
+
+    return formatResponse(false, null, {
+      type: "NETWORK_ERROR",
+      message: error.message,
+      status: null
+    });
+  }
+}
+
+
+const setupLoginForm =  () => {
+  loginForm.addEventListener("submit", async function(event) {
+
+    event.preventDefault();
+
+    const userId  = loginForm.elements["user-id"].value.trim();
+    const password  = loginForm.elements["password"].value.trim();
+    const userData = await getUserById(userId);
+    
+    if(!userData.success){
+      alert("Password or user id incorrect")
+      return;
+    }
+
+    if(password !== userData.data.data.password){
+      alert("Password or user id incorrect")
+      return;
+    }
+
+    localStorage.setItem("loggedUserId", userData.data.id)
+    window.location.href = "./profile.html"
+  })
+}
+
+setupLoginForm();
