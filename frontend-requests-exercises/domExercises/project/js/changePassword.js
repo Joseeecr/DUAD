49de0a642c006f11ee73bc014453a0a424defef3
Jose Blanco <blancomiguelj@gmail.com>
2026-04-11 14:06:55 -0600

    Finished change password html and js files

---
+++ frontend-requests-exercises/domExercises/project/js/changePassword.js
@@ -0,0 +1,108 @@
+const userInstance = axios.create({
+  baseURL: "https://api.restful-api.dev/",
+  headers:{
+    "x-api-key": "77adaabc-9a83-469f-baaa-9973f9b8a14c"
+  }
+});
+const changePasswordForm = document.getElementById("change-password-form");
+
+
+const formatResponse = (success, data, error) => {
+  return {
+    success,
+    data,
+    error
+  }
+}
+
+
+const getUserById = async (userId) => {
+  try {
+    const response = await userInstance.get(`collections/users/objects/${userId}`);
+    
+    return formatResponse(true, response.data, null);
+
+  } catch (error) {
+    if (error.response) {
+      const details = error.response?.data?.error || error
+
+      return formatResponse(false, null, {
+        type: "HTTP_ERROR",
+        message: error.response.statusText,
+        status: error.response.status,
+        details: details
+      });
+    }
+
+    return formatResponse(false, null, {
+      type: "NETWORK_ERROR",
+      message: error.message,
+      status: null
+    });
+  }
+}
+
+
+const updateUserData = async (userId, data) => {
+  try {
+    const response = await userInstance.patch(`collections/users/objects/${userId}`, {data});
+    return formatResponse(true, response.data, null);
+
+  } catch (error) {
+    if (error.response) {
+      return formatResponse(false, null, {
+        type: "HTTP_ERROR",
+        message: error.response.statusText,
+        status: error.response.status,
+        details: error.response.data.error
+      });
+    }
+
+    return formatResponse(false, null, {
+      type: "NETWORK_ERROR",
+      message: error.message,
+      status: null
+    });
+  }
+}
+
+
+
+const changePassword =  () => {
+  changePasswordForm.addEventListener("submit", async function(event) {
+
+    event.preventDefault();
+
+    const userId  = changePasswordForm.elements["userId"].value.trim();
+    const currentPassword  = changePasswordForm.elements["currentPassword"].value.trim();
+    const newPassword  = changePasswordForm.elements["newPassword"].value.trim();
+    const confirmPassword  = changePasswordForm.elements["confirmPassword"].value.trim();
+
+    const userData = await getUserById(userId);
+    
+    if(!userData.success){
+      alert("User id incorrect")
+      return;
+    }
+
+    if(currentPassword !== userData.data.data.password){
+      alert("Password incorrect")
+      return;
+    }
+
+    if(newPassword !== confirmPassword){
+      alert("Passwords do not match")
+      return;
+    }
+
+    await updateUserData(userId, {
+        email: userData.data.data.email,
+        password: newPassword,
+        address: userData.data.data.address
+    })
+
+    window.location.href = "./login.html"
+  })
+}
+
+changePassword();
