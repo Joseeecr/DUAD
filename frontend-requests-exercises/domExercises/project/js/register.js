3a4153f2567edbfe6a1e09f8d4ec09e5b80dc1f7
Jose Blanco <blancomiguelj@gmail.com>
2026-04-13 22:47:36 -0600

    Refactor: imported validation functions and created some other functions to handle errors individually instead of validating everything within the submit event

--- frontend-requests-exercises/domExercises/project/js/register.js
+++ frontend-requests-exercises/domExercises/project/js/register.js
@@ -1,105 +1,127 @@
 import { createUser } from "./services/userService.js";
 import { saveLoggedUserId } from "./auth/session.js";
+import { isEmpty, isEmailValid, isPasswordValid, doPasswordsMatch } from "./validations/validations.js";
 const form = document.getElementById("register-form");
+const errorMessage = document.getElementById("error-message");
 
 
-const isEmpty = (element) => {
-  return element.trim() === "";
+const showError = (message) => {
+  errorMessage.textContent = message;
 }
 
 
-const areFieldsValid = (inputValues) => {
+const clearError = () => {
+  errorMessage.textContent = "";
+}
+
+
+const validateRequiredFields = (inputValues) => {
   for(const item of inputValues){
     if(isEmpty(item.value)){
-      alert(`${item.label} should not be empty`);
-      return false;
+      return `${item.label} should not be empty`;
     }
   }
-  return true;
+  return null;
 }
 
 
-const isEmailValid = (email) => {
-  const validEmail = email.split("@");
+const getRegisterFormData = () => {
+  const name  = form.elements["name"].value.trim();
+  const email  = form.elements["email"].value.trim();
+  const password  = form.elements["password"].value.trim();
+  const confirmPassword  = form.elements["confirmPassword"].value.trim();
+  const address  = form.elements["address"].value.trim();
 
-  if((validEmail.length === 2) && (validEmail[0].length > 0) && (validEmail[1].length > 0)){
-    return true;
-  }
-
-  return false;
+  return {
+    name,
+    email,
+    password,
+    confirmPassword,
+    address
+  };
 }
 
 
-const isPasswordValid = (password) => {
-  if(password.length >= 8){
-    return true;
+const validateRegisterForm = ({name, email, password, confirmPassword, address}) => {
+  const inputValues = [
+    {label: "Name", value: name},
+    {label: "Email", value: email},
+    {label: "Password", value: password},
+    {label: "Confirm Password", value: confirmPassword},
+    {label: "Address", value: address},
+  ]
+
+  const fieldsError = validateRequiredFields(inputValues);
+
+  if(fieldsError){
+    return fieldsError;
   }
 
-  return false;
+  if(!isEmailValid(email)){
+    return "Invalid email structure. Use something like this 'email@example.com'";
+  }
+
+  if(!isPasswordValid(password)){
+    return "Password must be at least 8 characters long";
+  }
+
+  if(!doPasswordsMatch(password, confirmPassword)){
+    return "Passwords do not match";
+  }
+
+  return null;
 }
 
 
-const doPasswordsMatch = (password, confirmPassword) => {
-  if(password === confirmPassword){
-    return true;
+const handleRegisterError = (userData) => {
+
+  if(!userData.success && userData.error.type === "NETWORK_ERROR"){
+    return "Something went wrong";
   }
-  return false;
+
+  if(!userData.success){
+    return `An error occurred while creating the user: ${userData.error.details || userData.error.message}`;
+  }
+
+  return null;
+}
+
+
+const handleRegisterSuccess = (userData) => {
+  alert(`User created successfully! Your id is ${userData.data.id}`);
+  saveLoggedUserId(userData.data.id);
+  window.location.href = './profile.html';
+}
+
+
+const handleRegisterSubmit = async (event) => {
+  event.preventDefault();
+  clearError();
+
+  const formData = getRegisterFormData();
+
+  const error = validateRegisterForm(formData);
+
+  if(error){
+    showError(error);
+    return;
+  }
+
+  const userData = await createUser(formData.name, formData.email, formData.password, formData.address);
+
+  const registerError = handleRegisterError(userData);
+
+  if(registerError){
+    showError(registerError)
+    return;
+  }
+
+  handleRegisterSuccess(userData)
 }
 
 
 const setupRegisterForm =  () => {
-  form.addEventListener("submit", async function(event) {
-    event.preventDefault();
-    const name  = form.elements["name"].value.trim();
-    const email  = form.elements["email"].value.trim();
-    const password  = form.elements["password"].value.trim();
-    const confirmPassword  = form.elements["confirmPassword"].value.trim();
-    const address  = form.elements["address"].value.trim();
-
-    const inputValues = [
-      {label: "Name", value: name},
-      {label: "Email", value: email},
-      {label: "Password", value: password},
-      {label: "Confirm Password", value: confirmPassword},
-      {label: "Address", value: address},
-    ]
-
-    if(!areFieldsValid(inputValues)){
-      return;
-    }
-
-    if(!isEmailValid(email)){
-      alert("Invalid email structure. Use something like this 'email@example.com'")
-      return;
-    }
-
-    if(!isPasswordValid(password)){
-      alert("Password must be at least 8 characters long")
-      return;
-    }
-
-    if(!doPasswordsMatch(password, confirmPassword)){
-      alert("Passwords do not match")
-      return;
-    }
-
-    const userData = await createUser(name, email, password, address);
-
-
-    if(!userData.success && userData.error.type === "NETWORK_ERROR"){
-      alert("Something went wrong")
-      return;
-    }
-
-    if(!userData.success){
-      alert(`An error occurred while creating the user: ${user.error.details || user.error}`)
-      return;
-    }
-
-    alert(`User created successfully! Your id is ${userData.data.id}`)
-    saveLoggedUserId(userData.data.id)
-    window.location.href = './profile.html'
-  })
+  form.addEventListener("submit", handleRegisterSubmit)
 }
 
 setupRegisterForm();
