ed062cb7f7fb0a6513a97a92e54ee1eac1afff24
Jose Blanco <blancomiguelj@gmail.com>
2026-04-13 22:46:19 -0600

    Created to centralize validation functions

---
+++ frontend-requests-exercises/domExercises/project/js/validations/validations.js
@@ -0,0 +1,31 @@
+export const isEmpty = (element) => {
+  return element.trim() === "";
+}
+
+
+export const isEmailValid = (email) => {
+  const validEmail = email.split("@");
+
+  if((validEmail.length === 2) && (validEmail[0].length > 0) && (validEmail[1].length > 0)){
+    return true;
+  }
+
+  return false;
+}
+
+
+export const isPasswordValid = (password) => {
+  if(password.length >= 8){
+    return true;
+  }
+
+  return false;
+}
+
+
+export const doPasswordsMatch = (password, confirmPassword) => {
+  if(password === confirmPassword){
+    return true;
+  }
+  return false;
+}
