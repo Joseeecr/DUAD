aeb6227e7cfb81774fc5678c2a42a39b14e6768a
Jose Blanco <blancomiguelj@gmail.com>
2026-04-14 20:00:05 -0600

    Refactor: imported session function 'isUserLoggedIn' and created a couple of functions to divide better the responsabilities

--- frontend-requests-exercises/domExercises/project/js/profile.js
+++ frontend-requests-exercises/domExercises/project/js/profile.js
@@ -1,13 +1,8 @@
 import { getUserById } from "./services/userService.js";
-import { getLoggedUserId } from "./auth/session.js";
-import { removeLoggedUserId } from "./auth/session.js";
-
+import { getLoggedUserId, removeLoggedUserId, isUserLoggedIn } from "./auth/session.js";
+import { showErrorAlert } from "./ui/errorHandler.js"
 const userContainer = document.getElementById("user-container");
 const logoutBtn = document.getElementById("logout-btn");
-
-const isUserLoggedIn = () => {
-  return Boolean(localStorage.getItem("loggedUserId"));
-}
 
 
 const redirectIfNotLoggedIn = () => {
@@ -24,6 +19,29 @@ const logoutUser = () => {
   window.location.href = "./login.html";
 }
 
+
+const renderUserProfile = (userData) => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <h3>Name: ${userData.name}</h3> 
+    <p>Email: ${userData.data.email}</p>
+    <p>Address: ${userData.data.address}</p>
+  `;
+  userContainer.replaceChildren();
+  userContainer.append(card);
+}
+
+
+const handleUserProfileResult = (userResult) => {
+  if(userResult.success){
+    renderUserProfile(userResult.data);
+  } else {
+    showErrorAlert(`Error loading user data: ${userResult.error.details || userResult.error.message}`);
+  }
+}
+
+
 const protectProfilePage = async ()  => {
   if(!redirectIfNotLoggedIn()){
     return;
@@ -31,21 +49,9 @@ const protectProfilePage = async ()  => {
 
   const userId = getLoggedUserId();
 
-  const userData = await getUserById(userId);
+  const userResult = await getUserById(userId);
 
-  if(userData.success){
-  const card = document.createElement("div");
-  card.className = "card";
-  card.innerHTML = `
-  <h3>Name: ${userData.data.name}</h3> 
-  <p>Email: ${userData.data.data.email}</p>
-  <p>Address: ${userData.data.data.address}</p>
-  `;
-  userContainer.replaceChildren();
-  userContainer.append(card);
-  } else {
-    alert(`Error loading user data: ${userData.error.details || userData.error.message}`)
-  }
+  handleUserProfileResult(userResult);
 }
 
 
