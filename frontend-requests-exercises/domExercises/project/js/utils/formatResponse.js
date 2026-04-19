24c54ee29f3ceca9f6740ae9e5760835d6c2c106
Jose Blanco <blancomiguelj@gmail.com>
2026-04-19 11:08:22 -0600

    Created 'formatLabel' function to format error responses capitalizing the first letter and adding spaces between words

--- frontend-requests-exercises/domExercises/project/js/utils/formatResponse.js
+++ frontend-requests-exercises/domExercises/project/js/utils/formatResponse.js
@@ -5,3 +5,11 @@ export const formatResponse = (success, data, error) => {
     error
   };
 };
+
+
+export const formatLabel = (str) => {
+  return str
+    .replace(/([A-Z])/g, " $1")
+    .replace(/^./, (char) => char.toUpperCase())
+    .trim();
+};
