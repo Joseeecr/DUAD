2aa44f4f1a5854cc571a0e3b634ef09e09527000
Jose Blanco <blancomiguelj@gmail.com>
2026-05-30 14:29:24 -0600

    chore: Add cors configuration for frontend domains

--- lyfter-pet-store/backend_project/app/main.py
+++ lyfter-pet-store/backend_project/app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes.user_routes import user_bp
 from app.routes.products_routes import products_bp
 from app.routes.carts_routes import carts_bp
@@ -9,7 +10,8 @@ app.register_blueprint(user_bp)
 app.register_blueprint(products_bp)
 app.register_blueprint(carts_bp)
 app.register_blueprint(invoices_bp)
+CORS(app)
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="localhost", debug=True)
+    app.run(port=5000, host="127.0.0.1", debug=True)
