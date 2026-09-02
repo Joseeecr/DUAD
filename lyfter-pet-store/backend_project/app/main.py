e0da6e1dd354be1d3184e09f3e347203aa07c96e
Jose Blanco <blancomiguelj@gmail.com>
2026-09-01 22:36:38 -0600

    refactor: Add blueprint for products images route

--- lyfter-pet-store/backend_project/app/main.py
+++ lyfter-pet-store/backend_project/app/main.py
@@ -4,12 +4,15 @@ from app.routes.user_routes import user_bp
 from app.routes.products_routes import products_bp
 from app.routes.carts_routes import carts_bp
 from app.routes.invoices_routes import invoices_bp
+from app.routes.images_routes import images_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static")
+
 app.register_blueprint(user_bp)
 app.register_blueprint(products_bp)
 app.register_blueprint(carts_bp)
 app.register_blueprint(invoices_bp)
+app.register_blueprint(images_bp)
 CORS(app)
 
 
