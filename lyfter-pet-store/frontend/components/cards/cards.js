fcfbb9d7f306cb304ea2e56e0dde1bf3749cf16f
Jose Blanco <blancomiguelj@gmail.com>
2026-09-02 20:50:07 -0600

    refactor: Add div element with card-info to wrap the card information

--- lyfter-pet-store/frontend/components/cards/cards.js
+++ lyfter-pet-store/frontend/components/cards/cards.js
@@ -1,14 +1,16 @@
 import { cardTitle } from "./card-title.js"
 import { cardPrice } from "./card-price.js"
+import { cardImage } from "./card-image.js"
 
 export function createCard(props) {
   return `
     <div class="product-card">
-
-      ${cardTitle(props.card.title)}
-      ${cardPrice(props.card.price)}
-      <button>${props.card.button}</button>
-
+      ${cardImage(props.productImage)}
+      <div class="card-info">
+        ${cardTitle(props.card.title)}
+        ${cardPrice(props.card.price)}
+        <button class="btn see-details">${props.card.button}</button>
+      </div>
     </div>
   `
 }
